1d988b341cb78026496b769c6674732f904cb25c
enbysenpai <maria.nestor.classroom@gmail.com>
2025-01-27 17:39:24 +0200

    return data based on the accept header

--- watr_back/watr_back/controllers/alignment/alignment_controller.py
+++ watr_back/watr_back/controllers/alignment/alignment_controller.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, request, abort
 
 from watr_back.services.alignment.alignment_service import align as service_align
+from watr_back.services.alignment.alignment_service import convert_results_to_html as html_converter
+from watr_back.services.alignment.alignment_service import convert_results_to_jsonld as jsonld_converter
+
+
 
 alignment = Blueprint('alignment', __name__)
 
@@ -13,5 +17,16 @@ def align():
         abort(400, description="Invalid or missing 'target' parameter")
 
     results = service_align(target_ontology)
+
+    if request.accept_mimetype == "application/json":
+        return results
+
+    elif request.accept_mimetype == "text/html":
+        html_output = html_converter(results)
+        return html_output
+
+    elif request.accept_mimetype == "application/ld+json":
+        ld_json_output = jsonld_converter(results)
+        return ld_json_output
 
     return results
--- watr_back/watr_back/services/alignment/alignment_service.py
+++ watr_back/watr_back/services/alignment/alignment_service.py
@@ -1,11 +1,19 @@
+import json
 import subprocess
 import tempfile
+import xml.etree.ElementTree as ET
 
+import rdflib
 from SPARQLWrapper import SPARQLWrapper, TURTLE
 
 from watr_back.sparql_queries.alignment_queries import ALIGNMENT_QUERY
 
 sparql = SPARQLWrapper("http://localhost:3030/watr-dataset/sparql")
+
+NAMESPACES = {
+    'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    'alignment': 'http://knowledgeweb.semanticweb.org/heterogeneity/alignment'
+}
 
 def align(target_ontology):
     sparql_query = ALIGNMENT_QUERY
@@ -50,3 +58,86 @@ def get_path(target_ontology):
         return './ontologies/dbpedia.nt'
 
 
+
+def convert_results_to_html(align_data):
+    try:
+        tree = ET.parse(align_data)
+        root = tree.getroot()
+
+        alignment_data = []
+
+        for map_elem in root.findall('.//alignment:map', NAMESPACES):
+            for cell_elem in map_elem.findall('alignment:Cell', NAMESPACES):
+                entity1 = cell_elem.find('alignment:entity1', NAMESPACES).attrib.get(
+                    '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource', None)
+                entity2 = cell_elem.find('alignment:entity2', NAMESPACES).attrib.get(
+                    '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource', None)
+                measure = cell_elem.find('alignment:measure', NAMESPACES).text if cell_elem.find('alignment:measure',
+                                                                                                 NAMESPACES) is not None else None
+                relation = cell_elem.find('alignment:relation', NAMESPACES).text if cell_elem.find('alignment:relation',
+                                                                                                   NAMESPACES) is not None else None
+
+                alignment_data.append({
+                    'originalEntity': entity1,
+                    'alignedEntity': entity2,
+                    'measure': measure,
+                    'relation': relation
+                })
+
+        table_rows = ""
+        for row in alignment_data:
+            table_rows += f"""
+                    <tr>
+                        <td>{row['originalEntity']}</td>
+                        <td>{row['alignedEntity']}</td>
+                        <td>{row['measure']}</td>
+                        <td>{row['relation']}</td>
+                    </tr>
+                    """
+
+        html_template = f"""
+            <html>
+            <body>
+                <h1>Alignment Results</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Entity 1</th>
+                            <th>Entity 2</th>
+                            <th>Measure</th>
+                            <th>Relation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {table_rows}
+                    </tbody>
+                </table>
+            </body>
+            </html>
+            """
+
+        return html_template
+
+    except ET.ParseError as e:
+        print(f"The provided file is not XML: {e}")
+        return []
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return []
+
+
+def convert_results_to_jsonld(align_data):
+    try:
+        graph = rdflib.Graph()
+
+        graph.parse(align_data, format="xml")
+
+        jsonld_data = graph.serialize(format='json-ld', indent=4)
+
+        jsonld_dict = json.loads(jsonld_data)
+
+        return jsonld_dict
+
+    except Exception as e:
+        print(f"Error occurred while converting to JSON-LD: {e}")
+        return {}
